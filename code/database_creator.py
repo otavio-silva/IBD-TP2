80a14c7ad34d63743a234355046f68c995f4372b
Luiz Melo <luizhemelo@gmail.com>
2019-11-09 07:43:33 -0300

    Refactoring and deletion of some archives

--- code/database_creator.py
+++ code/database_creator.py
@@ -1,22 +1,25 @@
 import os
 import re
-import pandas
 import sqlalchemy
+import pandas as pd
 
 def get_create_table_string(tablename, connection):
 	sql = """
 	select * from sqlite_master where name = "{}" and type = "table"
 	""".format(tablename) 
 	result = connection.execute(sql)
+
 	return result.fetchmany()[0][4]
 
 def add_pk_to_create_table_string(create_table_string, colname):
 	regex = "(\n.+{}[^,]+)(,)".format(colname)
+
 	return re.sub(regex, "\\1 PRIMARY KEY,",  create_table_string)
 	
 def add_pk_to_sqlite_table(tablename, index_column, connection):
 	cts = get_create_table_string(tablename, connection)
 	cts = add_pk_to_create_table_string(cts, index_column)
+
 	template = """
 	BEGIN TRANSACTION;
 		ALTER TABLE {tablename} RENAME TO {tablename}_old_;
@@ -25,19 +28,20 @@ def add_pk_to_sqlite_table(tablename, index_column, connection):
 		DROP TABLE {tablename}_old_;
 	COMMIT TRANSACTION;
 	"""
+
 	create_and_drop_sql = template.format(tablename = tablename, cts = cts)
 	connection.executescript(create_and_drop_sql)
 
 if os.path.exists("database.db"):
 	os.remove("database.db")
-engine = sqlalchemy.create_engine("sqlite:///database.db", echo=False)
+engine = sqlalchemy.create_engine("sqlite:///aeronautical_occurrences_database.db", echo=False)
 
-anv = pandas.read_csv('../data/anv.csv', sep='~')
-ftc = pandas.read_csv('../data/ftc.csv', sep='~')
-oco = pandas.read_csv('../data/oco.csv', sep='~')
-rec = pandas.read_csv('../data/rec.csv', sep='~')
+anv = pd.read_csv('../data/anv.csv', sep='~')
+ftc = pd.read_csv('../data/ftc.csv', sep='~')
+oco = pd.read_csv('../data/oco.csv', sep='~')
+rec = pd.read_csv('../data/rec.csv', sep='~')
 
-anv.to_sql("AeronavesEnvolvidas", con=engine, if_exists='replace', index=False)
+anv.to_sql('AeronavesEnvolvidas', con=engine, if_exists='replace', index=False)
 ftc.to_sql('FatoresContribuintes', con=engine, if_exists='replace', index=False)
 oco.to_sql('Ocorrencias', con=engine, if_exists='replace', index=False)
 rec.to_sql('RecomendacoesSeguranca', con=engine, if_exists='replace', index=False)
